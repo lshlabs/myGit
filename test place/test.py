13ee20c1aaf77ed1d0ac64c687ca07a41dc32f10
Lee Sang Hun <tkdgns0964@gmail.com>
2024-05-01 06:47:26 +0900

    day14

--- test place/test.py
+++ test place/test.py
@@ -1,34 +1,65 @@
 import tkinter as tk
 from test_ui import setup_ui
 
-def show_frames():
-    for frame in frames[3:]:
-        frame.grid()
-    scrollbar.pack(side="right", fill='both')
-    title_frame.config(bg="#FA0150")
-    title_label.config(text="요기요", bg="#FA0150")
-
-def hide_frames():
+def menu1():
+    global labels
     for frame in frames[3:]:
         frame.grid_remove()
+    for label in labels[18:]:
+        label.grid_remove()
     scrollbar.pack_forget()
     title_frame.config(bg="#45D3D3")
     title_label.config(text="배달의 민족", bg="#45D3D3")
+    content_canvas.yview_moveto(0.0)    # 스크롤을 최상단으로 이동
+    scrollable_frame.unbind("<MouseWheel>")  # 마우스 휠 이벤트 바인딩 제거
+    
+def menu2():
+    global labels
+    for frame in frames[3:]:
+        frame.grid_remove()
+    for label in labels[18:]:
+        label.grid_remove()
+    scrollbar.pack_forget()
+    title_frame.config(bg="#FA0150")
+    title_label.config(text="요기요", bg="#FA0150")
+    content_canvas.yview_moveto(0.0)    # 스크롤을 최상단으로 이동
+    scrollable_frame.unbind("<MouseWheel>")  # 마우스 휠 이벤트 바인딩 제거
+
+def menu3():
+    global labels
+    for frame in frames[3:]:
+        frame.grid()
+    for label in labels[18:]:
+        label.grid()
+    scrollbar.pack(side="right", fill='both')
+    title_frame.config(bg="#ff6b00")
+    title_label.config(text="만나", bg="#ff6b00")
+    content_canvas.yview_moveto(0.0)    # 스크롤을 최상단으로 이동
+    scrollable_frame.bind("<MouseWheel>", on_mousewheel)  # 마우스 휠 이벤트 바인딩
+    
+def on_mousewheel(event):
+    # Windows 사용 시
+    content_canvas.yview_scroll(int(-1*(event.delta/120)), "units")
    
 if __name__ == "__main__":
     root = tk.Tk()
     root.geometry("600x400")
+    root.resizable(False, False)
     root.title('테스트 예제')
 
-    content_canvas, frames, menu_frame, uni_frame, scrollbar, title_frame, title_label = setup_ui(root)
+    content_canvas, frames, menu_frame, uni_frame, scrollbar, title_frame, title_label, scrollable_frame, labels = setup_ui(root)
     
     # 버튼 추가 및 기능 연결
-    button1 = tk.Button(menu_frame, text="메뉴 1", command=hide_frames)
+    button1 = tk.Button(menu_frame, text="배민", command=menu1)
     button1.pack(padx=10, pady=5, fill=tk.X)
 
-    button2 = tk.Button(menu_frame, text="메뉴 2", command=show_frames)
+    button2 = tk.Button(menu_frame, text="요기요", command=menu2)
     button2.pack(padx=10, pady=5, fill=tk.X)
+    
+    button3 = tk.Button(menu_frame, text="만나", command=menu3)
+    button3.pack(padx=10, pady=5, fill=tk.X)
 
-    hide_frames()
+
+    menu1()
 
     root.mainloop()
--- test place/test_ui.py
+++ test place/test_ui.py
@@ -16,14 +16,14 @@ def setup_ui(root):
     main_frame.columnconfigure(1, weight=1)  # main_frame의 두 번째 열이 확장 가능하게 설정
 
     dummy_frame = tk.Frame(main_frame)
-    dummy_frame.grid(row=0, column=1, padx=5, pady=5, sticky='nesw')
+    dummy_frame.grid(row=0, column=1, padx=(0,5), pady=5, sticky='nesw')
     
     # 타이틀 프레임 생성 및 배치
     title_frame = tk.Frame(dummy_frame, bg='#45D3D3', bd=1, relief='solid')
     title_frame.place(relwidth=1, relheight=0.2)
     
     # 타이틀 라벨 생성 및 배치
-    title_label = tk.Label(title_frame, text="배달의 민족", bg='#45D3D3', font=("default", 20, "bold"))
+    title_label = tk.Label(title_frame, text="flatform name", bg='#45D3D3', font=("default", 20, "bold"), fg='white')
     title_label.pack(pady=25, anchor='center')
     
     # 캔버스와 스크롤바를 품은 프레임
@@ -36,7 +36,7 @@ def setup_ui(root):
     
     scrollbar = tk.Scrollbar(uni_frame, orient='vertical', command=content_canvas.yview, highlightbackground='#d9d9d9')
     scrollbar.pack(side="right", fill='both')
-    scrollable_frame = tk.Frame(content_canvas)
+    scrollable_frame = tk.Frame(content_canvas, padx=10, pady=10)
     scrollable_frame.pack(side='right', fill='both')
     
     scrollable_frame.bind(
@@ -51,16 +51,18 @@ def setup_ui(root):
     # 파란색 프레임 추가
     frames = []
     for i in range(6):
-        frame = tk.Frame(scrollable_frame, bg='blue', width=100, height=100)
+        frame = tk.Frame(scrollable_frame, bg='grey', width=100, height=100, bd=1, relief='solid')
         frame.grid(row=i//3, column=i%3, padx=30, pady=15)
         frame.grid_propagate(True)
-        frames.append(frame)
+        frames.append(frame)    # 생성된 프레임을 리스트에 추가
 
     # 라벨 추가
-    for i in range(50):
+    labels = []
+    for i in range(51):
         label_row = 2 + i//3
         label_column = i%3
-        tk.Label(scrollable_frame, text="라벨 " + str(i)).grid(row=label_row, column=label_column, pady=3)
+        label = tk.Label(scrollable_frame, text="라벨 " + str(i))
+        label.grid(row=label_row, column=label_column, pady=3)
+        labels.append(label)  # 생성된 라벨을 리스트에 추가
 
-    scrollbar.set(0,0)
-    return content_canvas, frames, menu_frame, uni_frame, scrollbar, title_frame, title_label
+    return content_canvas, frames, menu_frame, uni_frame, scrollbar, title_frame, title_label, scrollable_frame, labels
