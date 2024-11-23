a9c84cd0f99a6c58cbf61a6a36c740c87b96d0d0
Lee Sang Hun <tkdgns0964@gmail.com>
2024-11-23 20:33:28 +0900

    day31

--- sampleproject/clicker_general.py
+++ sampleproject/clicker_general.py
@@ -1,6 +1,8 @@
 import os
 import time
 import webbrowser
+from typing import List
+from pathlib import Path
 
 from PIL import ImageGrab
 import cv2 as cv
@@ -8,64 +10,71 @@ import numpy as np
 import pyautogui as pg
 
 
-os.environ.pop("QT_QPA_PLATFORM_PLUGIN_PATH")  # Only if you get weir errors on startup
+os.environ.pop("QT_QPA_PLATFORM_PLUGIN_PATH", None)  # Only if you get weir errors on startup
 
 
 class ReflexClicker:
-    def __init__(self):
-        self.images = (
-            cv.imread("img01.png"),
-            cv.imread("img02.png"),
-            cv.imread("img03.png"),
-        )
+    def __init__(self, max_clicks: int = 5):
+        self.images = self._load_images()
         self.current_img = self.images[0]
-        self.started = 0
         self.states = [False, False, False]
         self.click_count = 0
+        self.max_clicks = max_clicks
+
+    def _load_images(self) -> List[np.ndarray]:
+        current_dir = Path(__file__).parent
+        images = []
+        for i in range(1, 4):
+            img_path = current_dir / f"img0{i}.png"
+            img = cv.imread(str(img_path))
+            if img is None:
+                raise FileNotFoundError(f"이미지를 찾을 수 없습니다: {img_path}")
+            images.append(img)
+        return images
 
     @staticmethod
-    def _get_image():
-        img = ImageGrab.grab()
-        img_cv = cv.cvtColor(np.array(img), cv.COLOR_RGB2BGR)
-        return img_cv
+    def _get_image() -> np.ndarray:
+        return cv.cvtColor(np.array(ImageGrab.grab()), cv.COLOR_RGB2BGR)
 
-    def _set_image_by_state(self):
+    def _set_image_by_state(self) -> None:
         for i, state in enumerate(self.states):
             if not state:
                 self.current_img = self.images[i]
-                break
+                return
 
-    def _update_state(self):
-        for i, _ in enumerate(self.states):
-            if not self.states[i]:
+    def _update_state(self) -> None:
+        for i, state in enumerate(self.states):
+            if not state:
                 if i == 1:
                     self.click_count += 1
                 self.states[i] = True
                 break
-        if all(s for s in self.states):
+        
+        if all(self.states):
             self.states = [True, False, False]
             time.sleep(2)
 
-    def _get_score(self, ss):
-        return cv.matchTemplate(ss, self.current_img, cv.TM_CCOEFF_NORMED)
+    def _get_score(self, screenshot: np.ndarray) -> np.ndarray:
+        return cv.matchTemplate(screenshot, self.current_img, cv.TM_CCOEFF_NORMED)
 
-    def run(self):
-        self._set_image_by_state()
-        ss = self._get_image()
-        res = self._get_score(ss)
+    def run(self) -> None:
+        while self.click_count < self.max_clicks:
+            self._set_image_by_state()
+            screenshot = self._get_image()
+            res = self._get_score(screenshot)
 
-        if (res >= 0.8).any():
-            h, w = self.current_img.shape[:-1]
-            loc = cv.minMaxLoc(res)[-1]
-            pg.moveTo(loc[0] + w // 2, loc[1] + h // 2)
-            self._update_state()
-            pg.click()
+            if (res >= 0.8).any():
+                h, w = self.current_img.shape[:-1]
+                loc = cv.minMaxLoc(res)[-1]
+                pg.moveTo(loc[0] + w // 2, loc[1] + h // 2)
+                self._update_state()
+                pg.click()
 
-        if self.click_count < 5:
-            self.run()
+def main():
+    webbrowser.open("https://humanbenchmark.com/tests/reactiontime")
+    time.sleep(5)
+    rc = ReflexClicker()
+    rc.run()
 
-
-webbrowser.open("https://humanbenchmark.com/tests/reactiontime")
-time.sleep(5)
-rc = ReflexClicker()
-rc.run()
+if __name__ == "__main__":
+    main()
